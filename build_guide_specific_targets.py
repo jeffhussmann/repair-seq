883ecebe9f81fe268e9ec0b80237f69a25be2686
Jeff Hussmann <jeff.hussmann@gmail.com>
2019-10-17 11:32:09 -0700

    fixes ommisions in build_guide_specific_targets

--- build_guide_specific_targets.py
+++ build_guide_specific_targets.py
@@ -16,10 +16,10 @@ def build_guide_specific_target(original_target, guide_library, guide_row):
 
     new_name = f'pooled_vector_{guide_library}_{guide}'
 
-    new_dir = original.dir.parent / new_name
+    new_dir = original_target.dir.parent / new_name
     new_dir.mkdir(exist_ok=True)
 
-    ps = original.features['pooled_vector', 'protospacer']
+    ps = original_target.features['pooled_vector', 'protospacer']
 
     gb = Bio.SeqIO.read('/home/jah/projects/ddr/targets/pooled_vector/pooled_vector.gb', 'genbank')
 
@@ -29,16 +29,17 @@ def build_guide_specific_target(original_target, guide_library, guide_row):
 
     for fn in ['phiX.gb', 'donors.gb', 'manifest.yaml']:
         try:
-            (new_dir / fn).symlink_to(original.dir / fn)
+            (new_dir / fn).symlink_to(original_target.dir / fn)
         except FileExistsError:
             pass
 
-    new_ti = target_info.TargetInfo(base_dir, new_name)
+    new_ti = target_info.TargetInfo(original_target.base_dir, new_name)
 
     new_ti.make_references()
     new_ti.identify_degenerate_indels()
 
 if __name__ == '__main__':
+    base_dir = '/home/jah/projects/ddr'
     original_target = target_info.TargetInfo(base_dir, 'pooled_vector')
 
     original_target.make_references()
