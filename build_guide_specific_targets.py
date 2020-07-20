2ebb262da245265522e7ebdd90b8fc2684e65f2e
jeffhussmann <jeff.hussmann@gmail.com>
2020-07-19 18:59:53 -0700

    generalizes some functionality in build_guide_specific_targets

--- build_guide_specific_targets.py
+++ build_guide_specific_targets.py
@@ -11,28 +11,30 @@ from hits import fasta, mapping_tools, utilities
 from ddr.guide_library import GuideLibrary
 
 def build_guide_specific_target(original_target,
+                                original_genbank_name,
                                 guide_library,
                                 guide,
                                 tasks_queue=None,
                                ):
     warnings.simplefilter('ignore')
 
-    new_name = f'pooled_vector_{guide_library.name}_{guide}'
+    new_name = f'{original_target.name}_{guide_library.name}_{guide}'
 
     new_dir = original_target.dir.parent / new_name
     new_dir.mkdir(exist_ok=True)
 
-    ps = original_target.features['pooled_vector', 'protospacer']
-
-    gb_fn = original_target.dir / f'{original_target.name}.gb'
+    gb_fn = original_target.dir / f'{original_genbank_name}.gb'
     gb = Bio.SeqIO.read(str(gb_fn), 'genbank')
 
+    protospacer = original_target.features[original_target.target, 'protospacer']
     ps_seq = guide_library.guides_df.loc[guide, 'protospacer']
-    gb.seq = gb.seq[:ps.start] + ps_seq + gb.seq[ps.end + 1:]
+    gb.seq = gb.seq[:protospacer.start] + ps_seq + gb.seq[protospacer.end + 1:]
 
-    Bio.SeqIO.write(gb, str(new_dir / f'{original_target.name}.gb'), 'genbank')
+    Bio.SeqIO.write(gb, str(new_dir / f'{original_genbank_name}.gb'), 'genbank')
 
-    for fn in ['phiX.gb', 'donors.gb', 'manifest.yaml']:
+    fns_to_copy = [f'{source}.gb' for source in original_target.sources]
+    fns_to_copy.append('manifest.yaml')
+    for fn in fns_to_copy:
         try:
             (new_dir / fn).symlink_to(original_target.dir / fn)
         except FileExistsError:
@@ -93,11 +95,10 @@ def build_doubles_guide_specific_target(original_target,
     if tasks_queue is not None:
         tasks_queue.put((fixed_guide, variable_guide))
 
-def build_all_singles():
+def build_all_singles(base_dir, original_target_name, original_genbank_name, guide_library_names):
     warnings.simplefilter('ignore')
 
-    base_dir = '/home/jah/projects/ddr'
-    original_target = target_info.TargetInfo(base_dir, 'pooled_vector')
+    original_target = target_info.TargetInfo(base_dir, original_target_name)
 
     original_target.make_references()
     original_target.identify_degenerate_indels()
@@ -107,26 +108,23 @@ def build_all_singles():
     manager = multiprocessing.Manager()
     tasks_done_queue = manager.Queue()
 
-    for guide_library_name in [
-        #'DDR_library',
-        #'DDR_sublibrary',
-        'DDR_microlibrary',
-    ]:
+    for guide_library_name in guide_library_names:
         guide_library = GuideLibrary(base_dir, guide_library_name)
         for guide in guide_library.guides:
-            args_list.append((original_target, guide_library, guide, tasks_done_queue))
-
-    #args_list = args_list[:10]
-    #for args in tqdm.tqdm(args_list):
-    #    build_guide_specific_target(*args)
+            args_list.append((original_target, original_genbank_name, guide_library, guide, tasks_done_queue))
 
     progress = tqdm.tqdm(desc='Making targets', total=len(args_list))
-    pool = multiprocessing.Pool(processes=18)
-    pool.starmap_async(build_guide_specific_target, args_list)
 
-    while progress.n != len(args_list):
-        tasks_done_queue.get()
-        progress.update()
+    #args_list = args_list[:10]
+    #for args in args_list:
+    #    build_guide_specific_target(*args)
+
+    with multiprocessing.Pool(processes=18) as pool:
+        pool.starmap_async(build_guide_specific_target, args_list)
+
+        while progress.n != len(args_list):
+            tasks_done_queue.get()
+            progress.update()
 
 def build_all_doubles():
     warnings.simplefilter('ignore')
@@ -168,7 +166,17 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    #if args.vector == 'singles':
+    #    base_dir = '/home/jah/projects/prime_editing_screens'
+    #    original_target = 'pPC1000_G6C_15'
+    #    original_genbank_name = 'ppc1000'
+    #    guide_library_names = ['DDR_library']
+    #    build_all_singles(base_dir, original_target, original_genbank_name, guide_library_names)
     if args.vector == 'singles':
-        build_all_singles()
+        base_dir = '/home/jah/projects/ddr'
+        original_target = 'pooled_vector'
+        original_genbank_name = 'pooled_vector'
+        guide_library_names = ['DDR_library', 'DDR_sublibrary']
+        build_all_singles(base_dir, original_target, original_genbank_name, guide_library_names)
     elif args.vector == 'doubles':
         build_all_doubles()
