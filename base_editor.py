c3999d4a9a428424615a0b0d99d2d7b1e248c42b
Jeff Hussmann <jeff.hussmann@gmail.com>
2020-11-06 18:13:21 -0500

    adds base_editor

---
+++ base_editor.py
@@ -0,0 +1,33 @@
+import functools
+from collections import Counter
+from hits import utilities
+import ddr.outcome
+
+def at_least_n_Bs(pool, n, B):
+    outcomes = []
+    b_rc = utilities.reverse_complement(B)
+    for c, s, d in pool.outcome_counts('perfect').index.values:
+        if c == 'mismatches':
+            outcome = ddr.outcome.MismatchOutcome.from_string(d)
+            if Counter(outcome.snvs.basecalls)[b_rc] >= n:
+                outcomes.append((c, s, d))
+    return outcomes
+
+def exactly_n_Bs(pool, n, B):
+    outcomes = []
+    b_rc = utilities.reverse_complement(B)
+    for c, s, d in pool.outcome_counts('perfect').index.values:
+        if c == 'mismatches':
+            outcome = ddr.outcome.MismatchOutcome.from_string(d)
+            if Counter(outcome.snvs.basecalls)[b_rc] == n:
+                outcomes.append((c, s, d))
+    return outcomes
+
+def at_least_n_Bs_curried(n, B):
+    return functools.partial(at_least_n_Bs, n=n, B=B)
+
+def exactly_n_Bs_curried(n, B):
+    return functools.partial(exactly_n_Bs, n=n, B=B)
+
+def get_mismatch_outcomes(pool):
+    return [(c, s, d) for c, s, d in pool.outcome_counts('perfect').index.values if c == 'mismatches']
