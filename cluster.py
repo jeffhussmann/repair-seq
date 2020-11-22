f44c30831ecb2e37ee733b88453e45e254babb80
Jeff Hussmann <jeff.hussmann@gmail.com>
2020-11-22 11:43:02 -0500

    removes old clustermap function

--- cluster.py
+++ cluster.py
@@ -1,7 +1,15 @@
 import numpy as np
 import scipy.cluster
 
-def cluster(pool, outcomes, guides, metric='correlation', method='single', fixed_guide='none', min_UMIs=None):
+def hierarchcical(
+    pool, outcomes, guides,
+    metric='correlation',
+    method='single',
+    fixed_guide='none',
+    min_UMIs=None,
+    num_clusters=5,
+):
+
     if isinstance(outcomes, int):
         outcomes = pool.most_frequent_outcomes(fixed_guide)[:outcomes]
 
@@ -28,7 +36,7 @@ def cluster(pool, outcomes, guides, metric='correlation', method='single', fixed
                                                       labels=guides,
                                                      )
 
-    guide_order = guide_dendro['ivl']
+    clustered_guide_order = guide_dendro['ivl']
     
     outcome_linkage = scipy.cluster.hierarchy.linkage(l2_fcs[guides],
                                                       optimal_ordering=True,
@@ -40,17 +48,27 @@ def cluster(pool, outcomes, guides, metric='correlation', method='single', fixed
                                                         labels=outcomes,
                                                        )
 
-    outcome_order = outcome_dendro['ivl']
+    clustered_outcome_order = outcome_dendro['ivl']
 
-    l2_fcs = l2_fcs.loc[outcome_order, guide_order]
+    l2_fcs = l2_fcs.loc[clustered_outcome_order, clustered_guide_order]
+
+    cluster_ids = scipy.cluster.hierarchy.fcluster(guide_linkage,
+                                                   criterion='maxclust',
+                                                   t=num_clusters,
+                                                  )
+    # cluster_ids is an array of cluster assignments in the original order.
+    # Transform into the order produced by dendrogram.
+    guide_cluster_assignments = [cluster_ids[guides.get_loc(g)] for g in clustered_guide_order]
 
     results = {
-        'guide_order': guide_order,
+        'clustered_guide_order': clustered_guide_order,
+        'guide_cluster_assignments': guide_cluster_assignments,
+
         'guide_correlations': l2_fcs.corr(),
         'guide_linkage': guide_linkage,
         'original_guide_order': guides,
 
-        'outcome_order': outcome_order,
+        'clustered_outcome_order': clustered_outcome_order,
         'outcome_correlations': l2_fcs.T.corr(),
     }
 
--- visualize/clustermap.py
+++ visualize/clustermap.py
@@ -13,240 +13,6 @@ import ddr.visualize.outcome_diagrams
 def clustermap(pool,
                outcomes=40,
                guides=100,
-               upside_down=False,
-               layout_kwargs=None,
-               fixed_guide='none',
-               cluster='both',
-               outcome_diagram='heatmap',
-               nt_guide_color='C1',
-               gene_to_color=None,
-               min_UMIs=None,
-               draw_colorbars=True,
-              ):
-
-    if layout_kwargs is None:
-        layout_kwargs = {}
-
-    if gene_to_color is None:
-        gene_to_color = {}
-
-    cmap = copy.copy(plt.get_cmap('PuOr_r'))
-    cmap.set_bad('white')
-
-    clustered_guide_order, clustered_outcome_order, correlations = ddr.cluster.cluster(pool,
-                                                                            outcomes,
-                                                                            guides,
-                                                                            method='average',
-                                                                            fixed_guide=fixed_guide,
-                                                                            min_UMIs=min_UMIs,
-                                                                            )
-
-    if cluster == 'both' or cluster == 'guides':
-        final_guide_order = clustered_guide_order
-    else:
-        final_guide_order = sorted(clustered_guide_order)
-
-    if cluster == 'both' or cluster == 'outcomes':
-        final_outcome_order = clustered_outcome_order
-    else:
-        final_outcome_order = outcomes
-
-    if upside_down:
-        mask = np.triu(np.ones((len(correlations['guides']), len(correlations['guides']))), k=1)
-    else:
-        mask = np.tri(len(correlations['guides']), k=-1)
-
-    masked = np.ma.array(correlations['guides'], mask=mask)
-
-    inches_per_guide = 16 / 100
-    corr_size_inches = len(final_guide_order) * inches_per_guide
-    fig, correlation_ax = plt.subplots(figsize=(corr_size_inches, corr_size_inches))
-
-    correlation_im = correlation_ax.imshow(masked, cmap=cmap, vmin=-1, vmax=1, interpolation='none')
-
-    correlation_ax.set_xticks([])
-    correlation_ax.set_yticks([])
-
-    for spine in correlation_ax.spines.values():
-        spine.set_visible(False)
-
-    transform = matplotlib.transforms.Affine2D().rotate_deg(-45) + correlation_ax.transData
-
-    correlation_im.set_transform(transform)
-
-    diag_length = np.sqrt(2 * len(correlations['guides'])**2)
-
-    if upside_down:
-        correlation_ax.set_ylim(diag_length / 2 + 1, 0)
-    else:
-        correlation_ax.set_ylim(0, -diag_length / 2 - 1)
-
-    correlation_ax.set_xlim(-np.sqrt(2) / 2, -np.sqrt(2) / 2 + diag_length)
-
-    for i, guide in enumerate(final_guide_order):
-        x = 2**0.5 * i
-        gene = pool.variable_guide_library.guide_to_gene[guide]
-        color = gene_to_color.get(gene, 'black')
-        correlation_ax.annotate(guide,
-                                xy=(x, 0),
-                                xytext=(0, 3 if upside_down else -3),
-                                textcoords='offset points',
-                                rotation=90,
-                                ha='center',
-                                va='bottom' if upside_down else 'top',
-                                size=7 if color == 'black' else 8,
-                                color=color,
-                                weight='normal' if color == 'black' else 'bold',
-                               )
-
-    if upside_down:
-        title_kwargs = dict(
-            xy=(0.5, 0),
-            xytext=(0, -15),
-            va='top',
-        )
-    else:
-        title_kwargs = dict(
-            xy=(0.5, 1),
-            xytext=(0, 15),
-            va='bottom',
-        )
-
-    if draw_colorbars:
-        correlation_ax.annotate(pool.group,
-                xycoords='axes fraction',
-                textcoords='offset points',
-                ha='center',
-                **title_kwargs,
-        )
-
-    corr_ax_p = correlation_ax.get_position()
-
-    if draw_colorbars:
-        # Draw correlation colorbar.
-        cbar_ax = fig.add_axes((corr_ax_p.x1 - corr_ax_p.width * 0.1, corr_ax_p.y0 + corr_ax_p.height * 0.4, corr_ax_p.width * 0.02, corr_ax_p.height * 0.4))
-        cbar = fig.colorbar(correlation_im, cax=cbar_ax, ticks=[-1, 0, 1])
-        cbar_ax.annotate('correlation\nbetween\noutcome\nredistribution\nprofiles',
-                        xy=(1, 0.5),
-                        xycoords='axes fraction',
-                        xytext=(60, 0),
-                        textcoords='offset points',
-                        ha='center',
-                        va='center',
-                        )
-
-        cbar.outline.set_alpha(0.1)
-
-    heatmap_width = corr_ax_p.width
-    heatmap_height = heatmap_width * len(final_outcome_order) / len(final_guide_order)
-    # Make gap equal to height for 7 outcome rows.
-    gap = heatmap_height / len(final_outcome_order) * 6
-
-    if upside_down:
-        y0 = corr_ax_p.y1 + gap
-    else:
-        y0 = corr_ax_p.y0 - heatmap_height - gap
-
-    fc_ax = fig.add_axes((corr_ax_p.x0, y0, heatmap_width, heatmap_height))
-
-    fcs = pool.log2_fold_changes('perfect', fixed_guide)[fixed_guide].loc[final_outcome_order, final_guide_order]
-
-    heatmap_kwargs = dict(cmap=plt.get_cmap('RdBu_r'), vmin=-2, vmax=2, interpolation='none')
-    heatmap_im = fc_ax.imshow(fcs, **heatmap_kwargs)
-
-    fc_ax.axis('off')
-
-    fc_ax_p = fc_ax.get_position()
-
-    window_start, window_end = layout_kwargs['window']
-    window_size = window_end - window_start + 1
-
-    # want 1 nt of diagram to be 3/4ths as wide as 1 guide of heatmap
-    diagram_width = fc_ax_p.width * window_size / len(final_guide_order) * 0.75
-    diagram_gap = diagram_width * 0.02
-
-    diagram_ax = fig.add_axes((fc_ax_p.x0 - diagram_width - diagram_gap, fc_ax_p.y0, diagram_width, fc_ax_p.height), sharey=fc_ax)
-    _ = ddr.visualize.outcome_diagrams.plot(final_outcome_order[::-1],
-                              pool.target_info,
-                              ax=diagram_ax,
-                              **layout_kwargs,
-                             )
-
-    diagram_ax.set_title('distance from cut site (nts)', size=10, pad=15)
-
-    outcome_corr_ax = fig.add_axes((fc_ax_p.x0, fc_ax_p.y0, fc_ax_p.height, fc_ax_p.height))
-
-    for side in ['left', 'top', 'bottom']:
-        outcome_corr_ax.spines[side].set_visible(False)
-
-    outcome_corr_ax.spines['right'].set_color('white')
-
-    if outcome_diagram == 'heatmap' and (cluster == 'both' or cluster == 'outcomes'):
-        mask = np.tri(len(correlations['outcomes']), k=-1)
-        masked = np.ma.array(correlations['outcomes'], mask=mask)
-
-        correlation_im = outcome_corr_ax.imshow(masked, cmap=cmap, vmin=-1, vmax=1, interpolation='none')
-
-        transform = matplotlib.transforms.Affine2D().rotate_deg(45) + outcome_corr_ax.transData
-        correlation_im.set_transform(transform)
-
-    outcome_corr_ax.set_xticks([])
-    outcome_corr_ax.set_yticks([])
-
-    diagonal_length = np.sqrt(2 * len(correlations['outcomes'])**2)
-
-    outcome_corr_ax.set_xlim(0, diagonal_length / 2)
-    outcome_corr_ax.set_ylim(-np.sqrt(2) / 2, -np.sqrt(2) / 2 + diagonal_length)
-
-    # Can't figure out how to prevent re-positioning of the axes, so simply reset it
-    # to the intended position after done.
-    outcome_corr_ax.set_position((fc_ax_p.x1 + 0.01 * fc_ax_p.width, fc_ax_p.y0, fc_ax_p.height / 2, fc_ax_p.height))
-
-    # Draw non-targeting fractions.
-
-    frequency_width = fc_ax_p.width * 0.15 * 100 / guides
-    frequency_gap = frequency_width * 0.1
-
-    diagram_ax_p = diagram_ax.get_position()
-
-    frequency_ax = fig.add_axes((diagram_ax_p.x0 - frequency_gap - frequency_width, fc_ax_p.y0, frequency_width, fc_ax_p.height), sharey=fc_ax)
-
-    frequencies = pool.non_targeting_fractions('perfect', 'none').loc[final_outcome_order]
-
-    xs = np.log10(frequencies)
-    ys = np.arange(len(frequencies))
-    frequency_ax.plot(xs, ys, '.', markeredgewidth=0, markersize=10, alpha=0.9, clip_on=False, color='black')
-
-    x_lims = np.log10(np.array([2e-3, 2e-1]))
-
-    for exponent in [3, 2, 1]:
-        xs = np.log10(np.arange(1, 10) * 10**-exponent)        
-        for x in xs:
-            if x_lims[0] <= x <= x_lims[1]:
-                frequency_ax.axvline(x, color='black', alpha=0.07, clip_on=False)
-
-    x_ticks = [x for x in [2e-3, 5e-3, 1e-2, 5e-2, 1e-1, 2e-1] if x_lims[0] <= np.log10(x) <= x_lims[1]]
-
-    frequency_ax.set_xticks(np.log10(x_ticks))
-    frequency_ax.set_xticklabels([f'{100 * x:g}' for x in x_ticks])
-
-    for side in ['left', 'right', 'bottom']:
-        frequency_ax.spines[side].set_visible(False)
-
-    frequency_ax.xaxis.tick_top()
-    frequency_ax.set_xlim(*x_lims)
-    frequency_ax.invert_xaxis()
-
-    frequency_ax.set_title('percentage of outcomes\nin cells containing\nnon-targeting guides', size=10, pad=15)
-
-    if draw_colorbars:
-        ddr.visualize.heatmap.add_fold_change_colorbar(fig, heatmap_im, -0.05, 0.4, 0.15, 0.02)
-
-    return fig, final_guide_order, final_outcome_order
-
-def clustermap_new(pool,
-               outcomes=40,
-               guides=100,
                diagram_ax_rectangle=None,
                fig=None,
                upside_down=False,
@@ -272,8 +38,7 @@ def clustermap_new(pool,
 
     fc_cmap = plt.get_cmap('RdBu_r')
 
-    #clustered_guide_order, clustered_outcome_order, correlations = ddr.cluster.cluster(pool,
-    cluster_results = ddr.cluster.cluster(pool,
+    cluster_results = ddr.cluster.hierarchcical(pool,
                                           outcomes,
                                           guides,
                                           method='average',
@@ -282,19 +47,19 @@ def clustermap_new(pool,
                                          )
 
     if cluster == 'both' or cluster == 'guides':
-        final_guide_order = cluster_results['guide_order']
+        final_guide_order = cluster_results['clustered_guide_order']
     else:
-        final_guide_order = sorted(cluster_results['guide_order'])
+        final_guide_order = sorted(cluster_results['clustered_guide_order'])
 
     if cluster == 'both' or cluster == 'outcomes':
-        final_outcome_order = cluster_results['outcome_order']
+        final_outcome_order = cluster_results['clustered_outcome_order']
     else:
         final_outcome_order = outcomes
 
     num_guides = len(final_guide_order)
     num_outcomes = len(final_outcome_order)
 
-    window_start, window_end = layout_kwargs['window']
+    window_start, window_end = layout_kwargs.get('window', (-30, 30))
     window_size = window_end - window_start + 1
 
     inches_per_guide = 14 / 100
@@ -477,20 +242,14 @@ def clustermap_new(pool,
         guide_clusters_rectangle = (guide_clusters_x0, guide_clusters_y0, guide_clusters_width, guide_clusters_height)
         guide_clusters_ax = fig.add_axes(guide_clusters_rectangle)
 
-        indices = [cluster_results['original_guide_order'].get_loc(g) for g in cluster_results['guide_order']]
-
-        cluster_ids = scipy.cluster.hierarchy.fcluster(cluster_results['guide_linkage'], criterion='maxclust', t=num_clusters)
-
-        ordered_cluster_ids = [cluster_ids[i] for i in indices]
-
-        num_clusters = len(set(ordered_cluster_ids))
+        num_clusters = len(set(cluster_results['guide_cluster_assignments']))
 
         palette = sns.husl_palette(num_clusters)
 
         id_to_color = {i: palette[i - 1] for i in range(1, num_clusters + 1)}
 
         for cluster_id in range(1, num_clusters + 1):
-            for x, v in enumerate(ordered_cluster_ids):
+            for x, v in enumerate(cluster_results['guide_cluster_assignments']):
                 if v == cluster_id:
                     guide_clusters_ax.plot([x, x + 1], [cluster_id - 1, cluster_id - 1],
                                            color=id_to_color[cluster_id],
