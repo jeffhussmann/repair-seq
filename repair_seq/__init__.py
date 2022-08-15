a690ce22060e6484a095f7e4ed3355915fa16f4d
Jeff Hussmann <jeff.hussmann@gmail.com>
2022-08-15 13:16:30 -0400

    removes cluster and replicates imports for performance reasons

--- repair_seq/__init__.py
+++ repair_seq/__init__.py
@@ -1,8 +1,6 @@
 __version__ = '1.0.4'
 
 from . import pooled_screen
-from . import cluster
 from . import guide_library
-from . import replicates
 from . import pooled_layout
 from . import arrayed_experiment_group
